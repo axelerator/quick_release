ea34c058bbc43bcb99f9ff34b3edfaf7e1e73d0e
Axel Tetzlaff <axel.tetzlaff@gmx.de>
2014-09-20 16:41:11 +0200

    fixed ratios

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ class InputImage {
 
   float ratio() {
     if (this->state == NEW) load();
-    return (float)height/width;
+    return (float)width/height;
   }
 
   private:
@@ -65,15 +65,10 @@ class InputImage {
     glBindTexture(GL_TEXTURE_2D, textureID);
 
     // Give the image to OpenGL
-    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 
     // OpenGL has now copied the data. Free our own version
     delete [] data;
-
-    // Poor filtering, or ...
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
     // ... nice trilinear filtering.
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -374,7 +369,7 @@ void InitGL()
         exit(1);
 
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    window = glfwCreateWindow(1280, 720, "ImGui OpenGL example", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "Quick Release", NULL, NULL);
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, glfw_key_callback);
     glfwSetScrollCallback(window, glfw_scroll_callback);
@@ -450,161 +445,205 @@ void UpdateImGui()
     ImGui::NewFrame();
 }
 
-void writePng(int x, int y, unsigned char *pixels) {
-  // PNG format
- // stbi_write_png("img.png", x, y, 3, pixels, 0);
-    stbi_write_png("foo.png", x, y, 3, pixels, 0);
-}
+class Rect {
+  public:
+  Rect(unsigned int width, unsigned int height):_width(width), _height(height){}
 
-void writeBMP(unsigned char* data, unsigned int w, unsigned int h) {
-  unsigned char *img = NULL;
-  int filesize = 54 + 3*w*h;  //w is your image width, h is image height, both int
+  unsigned int width() {return _width;}
+  unsigned int height() {return _height;}
 
-  unsigned char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0};
-  unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
-  unsigned char bmppad[3] = {0,0,0};
+  float ratio() const {return (float)_width/_height;}
+  bool landscape()  const{return ratio() >= 1.0; }
+  private:
+    const unsigned int _width;
+    const unsigned int _height;
 
-  bmpfileheader[ 2] = (unsigned char)(filesize    );
-  bmpfileheader[ 3] = (unsigned char)(filesize>> 8);
-  bmpfileheader[ 4] = (unsigned char)(filesize>>16);
-  bmpfileheader[ 5] = (unsigned char)(filesize>>24);
+};
 
-  bmpinfoheader[ 4] = (unsigned char)(       w    );
-  bmpinfoheader[ 5] = (unsigned char)(       w>> 8);
-  bmpinfoheader[ 6] = (unsigned char)(       w>>16);
-  bmpinfoheader[ 7] = (unsigned char)(       w>>24);
-  bmpinfoheader[ 8] = (unsigned char)(       h    );
-  bmpinfoheader[ 9] = (unsigned char)(       h>> 8);
-  bmpinfoheader[10] = (unsigned char)(       h>>16);
-  bmpinfoheader[11] = (unsigned char)(       h>>24);
+class Geometry {
+  public:
 
-  FILE *f = fopen("img.bmp","wb");
-  fwrite(bmpfileheader,1,14,f);
-  fwrite(bmpinfoheader,1,40,f);
-  for(int i=0; i<h; i++)
-  {
-      fwrite(data+(w*(h-i-1)*3),3,w,f);
-      fwrite(bmppad,1,(4-(w*3)%4)%4,f);
-  }
-  fclose(f);
-}
+    Geometry(InputImage &image, const Rect &screen): inputImage(image), screen(screen) {
+      shaderProgramId = LoadShaders("vertex.vertexshader", "fragment.fragmentshader");
+      exposureUniform = glGetUniformLocation(shaderProgramId,"level");
+
+      // Get a handle for our buffers
+      vertexPosition_modelspaceID = glGetAttribLocation(shaderProgramId, "vertexPosition_modelspace");
+      vertexUVID = glGetAttribLocation(shaderProgramId, "vertexUV");
+
+      // An array of 3 vectors which represents 3 vertices
+      float r = screen.ratio() / image.ratio(); //(1.0 / image.ratio()) *0.5;
+      static const GLfloat g_vertex_buffer_data[] = {
+       -1.0f, 1.0f * r, 0.0f,
+       -1.0f,-1.0f * r, 0.0f,
+        1.0f,-1.0f * r, 0.0f,
+        1.0f, 1.0f * r, 0.0f
+      };
+      // Two UV coordinatesfor each vertex. They were created with Blender. You'll learn shortly how to do this yourself.
+      static const GLfloat g_uv_buffer_data[] = {
+        0.0f, 0.0f,
+        0.0f, 1.0f,
+        1.0f, 1.0f,
+        1.0f, 0.0f
+      };
+
+      // Generate 1 buffer, put the resulting identifier in vertexbuffer
+      glGenBuffers(1, &vertexbuffer);
+      // The following commands will talk about our 'vertexbuffer' buffer
+      glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+      // Give our vertices to OpenGL.
+      glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+      glGenBuffers(1, &uvbuffer);
+      glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+      glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+      glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+      // Get a handle for our "myTextureSampler" uniform
+      textureUniform  = glGetUniformLocation(shaderProgramId, "myTextureSampler");
+    }
+
+    void draw(float exposure) {
+      // Use our shader
+      glUseProgram(shaderProgramId);
+
+      glUniform1f(exposureUniform, exposure);
+      // Set our "myTextureSampler" sampler to user Texture Unit 0
+      glUniform1i(textureUniform, 0);
+
+      // Bind our texture in Texture Unit 0
+      glActiveTexture(GL_TEXTURE0);
+      glBindTexture(GL_TEXTURE_2D, inputImage.textureId());
+
+      // 1rst attribute buffer : vertices
+      glEnableVertexAttribArray(vertexPosition_modelspaceID);
+      glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+      glVertexAttribPointer(
+          vertexPosition_modelspaceID,  // The attribute we want to configure
+          3,                            // size
+          GL_FLOAT,                     // type
+          GL_FALSE,                     // normalized?
+          0,                            // stride
+          (void*)0                      // array buffer offset
+          );
+
+      // 2nd attribute buffer : UVs
+      glEnableVertexAttribArray(vertexUVID);
+      glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+      glVertexAttribPointer(
+          vertexUVID,                   // The attribute we want to configure
+          2,                            // size : U+V => 2
+          GL_FLOAT,                     // type
+          GL_FALSE,                     // normalized?
+          0,                            // stride
+          (void*)0                      // array buffer offset
+          );
+
+      // Draw the triangles !
+      glDrawArrays(GL_QUADS, 0, 4*3); // 12*3 indices starting at 0 -> 12 triangles
+
+      glDisableVertexAttribArray(vertexPosition_modelspaceID);
+      glDisableVertexAttribArray(vertexUVID);
+      glBindBuffer(GL_ARRAY_BUFFER, 0);
+      glUseProgram(0);
+    }
+
+    void writeToDisk(string &path, float exposure, unsigned int width, unsigned int height) {
+      // PREP for render2text ------------------------------
+      // The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
+      GLuint FramebufferName = 0;
+      glGenFramebuffers(1, &FramebufferName);
+      glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+      // The depth buffer
+      if ( !GLEW_ARB_framebuffer_object ){ // OpenGL 2.1 doesn't require this, 3.1+ does
+        printf("Your GPU does not provide framebuffer objects. Use a texture instead.");
+        return;
+      }
+      GLuint depthrenderbuffer;
+      glGenRenderbuffers(1, &depthrenderbuffer);
+
+      // The texture we're going to render to
+      GLuint renderedTexture;
+      glGenTextures(1, &renderedTexture);
+
+      // "Bind" the newly created texture : all future texture functions will modify this texture
+      glBindTexture(GL_TEXTURE_2D, renderedTexture);
+
+      // Give an empty image to OpenGL ( the last "0" means "empty" )
+      glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0,GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+      glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
+      glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
+      glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
+
+      // Set "renderedTexture" as our colour attachement #0
+      glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderedTexture, 0);
+
+      // Set the list of draw buffers.
+      GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+      glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
+
+      // Always check that our framebuffer is ok
+      if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        return ;
+      // PREP for render2text END ------------------------------
+      //
+      // Render to our framebuffer
+      glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+      glViewport(0,0,width,height); // Render on the whole framebuffer, complete from the lower left corner to the upper right
+      // Rendering
+      glViewport(0, 0, width, height);
+      glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+      //
+      this->draw(exposure);
+
+      unsigned char *pixel_data = new unsigned char [width * height * 3];
+      glReadPixels(0,0,width,height, GL_RGB, GL_UNSIGNED_BYTE, pixel_data);
+
+      stbi_write_png(path.c_str(), width, height, 3, pixel_data, 0);
+      free(pixel_data);
+      // Render to display frame buffer again and free resources
+      glBindFramebuffer(GL_FRAMEBUFFER, 0);
+      GLuint deleteBuffers[] = {FramebufferName};
+      glDeleteFramebuffers(1, deleteBuffers);
+      GLuint deleteTexture[] = {renderedTexture};
+      glDeleteTextures(1, deleteTexture);
+      GLuint deleteBuffer[] = {depthrenderbuffer};
+      glDeleteRenderbuffers(1, deleteBuffer);
+    }
+
+  private:
+    InputImage &inputImage;
+    const Rect &screen;
+
+    GLuint shaderProgramId;
+    GLuint exposureUniform;
+    GLuint textureUniform;
+    GLuint vertexPosition_modelspaceID ;
+    GLuint vertexUVID;
+    GLuint vertexbuffer;
+    GLuint uvbuffer;
+
+};
+
 
 // Application code
 int main(int argc, char** argv)
 {
-    InitGL();
-    GLuint shaderProgramId = LoadShaders("vertex.vertexshader", "fragment.fragmentshader");
-    GLuint l;
-
-    l = glGetUniformLocation(shaderProgramId,"level");
-
-  // Get a handle for our buffers
-  GLuint vertexPosition_modelspaceID = glGetAttribLocation(shaderProgramId, "vertexPosition_modelspace");
-  GLuint vertexUVID = glGetAttribLocation(shaderProgramId, "vertexUV");
-  InputImage ttt("tesla2.jpg");
-
-
-  // An array of 3 vectors which represents 3 vertices
-  float r = (1.0 / ttt.ratio()) *0.5;
-  static const GLfloat g_vertex_buffer_data[] = {
-    -0.5f,  0.5f * r, 0.0f,
-    -0.5f, -0.5f * r, 0.0f,
-     0.5f, -0.5f * r, 0.0f,
-     0.5f,  0.5f * r, 0.0f,
-  };
-  // Two UV coordinatesfor each vertex. They were created with Blender. You'll learn shortly how to do this yourself.
-  static const GLfloat g_uv_buffer_data[] = {
-    0.0f, 0.0f,
-    0.0f, 1.0f,
-    1.0f, 1.0f,
-    1.0f, 0.0f
-  };
-
-  // This will identify our vertex buffer
-  GLuint vertexbuffer;
-  // Generate 1 buffer, put the resulting identifier in vertexbuffer
-  glGenBuffers(1, &vertexbuffer);
-  // The following commands will talk about our 'vertexbuffer' buffer
-  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-  // Give our vertices to OpenGL.
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-  GLuint uvbuffer;
-  glGenBuffers(1, &uvbuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  GLuint Texture = ttt.textureId();//loadBmp("uvtemplate.bmp");
-
-  // Get a handle for our "myTextureSampler" uniform
-  GLuint TextureID  = glGetUniformLocation(shaderProgramId, "myTextureSampler");
+  InitGL();
   static float foo = 1.0f;
-
-  // PREP for render2text ------------------------------
-  // The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
-	GLuint FramebufferName = 0;
-	glGenFramebuffers(1, &FramebufferName);
-	glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
-
-	// The texture we're going to render to
-	GLuint renderedTexture;
-	glGenTextures(1, &renderedTexture);
-
-	// "Bind" the newly created texture : all future texture functions will modify this texture
-	glBindTexture(GL_TEXTURE_2D, renderedTexture);
-
-	// Give an empty image to OpenGL ( the last "0" means "empty" )
-	glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, 1280, 1024, 0,GL_RGB, GL_UNSIGNED_BYTE, 0);
-
-	// Poor filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-	// The depth buffer
-	if ( !GLEW_ARB_framebuffer_object ){ // OpenGL 2.1 doesn't require this, 3.1+ does
-		printf("Your GPU does not provide framebuffer objects. Use a texture instead.");
-		return -1;
-	}
-	GLuint depthrenderbuffer;
-	glGenRenderbuffers(1, &depthrenderbuffer);
-	glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 1280, 1024);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
-
-	//// Alternative : Depth texture. Slower, but you can sample it later in your shader
-	//GLuint depthTexture;
-	//glGenTextures(1, &depthTexture);
-	//glBindTexture(GL_TEXTURE_2D, depthTexture);
-	//glTexImage2D(GL_TEXTURE_2D, 0,GL_DEPTH_COMPONENT24, 1024, 768, 0,GL_DEPTH_COMPONENT, GL_FLOAT, 0);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	//glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthTexture, 0);
+  int w, h;
+  glfwGetWindowSize(window, &w, &h);
+  const Rect screen(w, h);
 
 
-	// Set "renderedTexture" as our colour attachement #0
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderedTexture, 0);
-
-	// Set the list of draw buffers.
-	GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-	glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
-
-	// Always check that our framebuffer is ok
-	if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-		return false;
-  // PREP for render2text END ------------------------------
-  // Render to display frame buffer
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-  //
-  // Render to our framebuffer
-  //glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
-  //glViewport(0,0,1280,1024); // Render on the whole framebuffer, complete from the lower left corner to the upper right
-
-
-  //InputImage tesla("tesla.jpg");
+  InputImage tesla("fo_o.png");
+  Geometry g(tesla, screen);
   InitImGui();
   bool save_now = false;
 
@@ -660,62 +699,18 @@ int main(int argc, char** argv)
 
         // Rendering
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        glClearColor(0.8f, 0.6f, 0.6f, 1.0f);
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        { // drawing the textured quad
-            // Use our shader
-            glUseProgram(shaderProgramId);
-            glUniform1f(l, foo);
-
-            // Bind our texture in Texture Unit 0
-            glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, Texture);
-            // Set our "myTextureSampler" sampler to user Texture Unit 0
-            glUniform1i(TextureID, 0);
-
-            // 1rst attribute buffer : vertices
-            glEnableVertexAttribArray(vertexPosition_modelspaceID);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-            glVertexAttribPointer(
-                    vertexPosition_modelspaceID,  // The attribute we want to configure
-                    3,                            // size
-                    GL_FLOAT,                     // type
-                    GL_FALSE,                     // normalized?
-                    0,                            // stride
-                    (void*)0                      // array buffer offset
-            );
-
-            // 2nd attribute buffer : UVs
-            glEnableVertexAttribArray(vertexUVID);
-            glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-            glVertexAttribPointer(
-                    vertexUVID,                   // The attribute we want to configure
-                    2,                            // size : U+V => 2
-                    GL_FLOAT,                     // type
-                    GL_FALSE,                     // normalized?
-                    0,                            // stride
-                    (void*)0                      // array buffer offset
-            );
-
-            // Draw the triangles !
-            glDrawArrays(GL_QUADS, 0, 4*3); // 12*3 indices starting at 0 -> 12 triangles
-
-            glDisableVertexAttribArray(vertexPosition_modelspaceID);
-            glDisableVertexAttribArray(vertexUVID);
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            glUseProgram(0);
-        }
-
+        g.draw(foo);
         ImGui::Render();
         glfwSwapBuffers(window);
     }
-    //unsigned char *pixel_data = new unsigned char [1280 * 1024 * 3];
-    //glReadPixels(0,0,1280,1024, GL_RGB, GL_UNSIGNED_BYTE, pixel_data);
-
+    std::string f("foo.png");
+    g.writeToDisk(f, foo, 1280, 1024);
     ImGui::Shutdown();
     glfwTerminate();
-    //writeBMP(pixel_data, 1280, 1024);
+
 
     return 0;
 }
